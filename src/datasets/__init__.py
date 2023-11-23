99b52ee6c7895eddf66326a87834d497a8db1bf1
tsessk <tsessk@192.168.1.7>
2023-11-23 12:02:43 +0300

    fix datasets

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -1,13 +1,5 @@
-from hw_asr.datasets.custom_audio_dataset import CustomAudioDataset
-from hw_asr.datasets.custom_dir_audio_dataset import CustomDirAudioDataset
-from hw_asr.datasets.librispeech_dataset import LibrispeechDataset
-from hw_asr.datasets.ljspeech_dataset import LJspeechDataset
-from hw_asr.datasets.common_voice import CommonVoiceDataset
+from src.datasets.ljspeech import LJSpeechDataset
 
 __all__ = [
-    "LibrispeechDataset",
-    "CustomDirAudioDataset",
-    "CustomAudioDataset",
-    "LJspeechDataset",
-    "CommonVoiceDataset"
+    'LJSpeechDataset'
 ]
