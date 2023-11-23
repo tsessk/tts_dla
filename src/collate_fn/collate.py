3ff937724a1a615f32b63b7d3ee3daf51b478321
tsessk <tsessk@192.168.1.7>
2023-11-23 22:56:58 +0300

    fix collate mb

--- src/collate_fn/collate.py
+++ src/collate_fn/collate.py
@@ -74,11 +74,11 @@ def pad_2D_tensor(inputs, maxlen=None):
 
 
 def reprocess_tensor(batch, cut_list):
-    texts = [batch[ind]["src_seq"] for ind in cut_list]
+    texts = [batch[ind]["text"] for ind in cut_list]
     mel_targets = [batch[ind]["mel_target"] for ind in cut_list]
-    durations = [batch[ind]["duration_target"] for ind in cut_list]
-    pitches = [batch[ind]["pitch_target"] for ind in cut_list]
-    energies = [batch[ind]["energy_target"] for ind in cut_list]
+    durations = [batch[ind]["duration"] for ind in cut_list]
+    pitches = [batch[ind]["pitch"] for ind in cut_list]
+    energies = [batch[ind]["energy"] for ind in cut_list]
 
     length_text = np.array([])
     for text in texts:
